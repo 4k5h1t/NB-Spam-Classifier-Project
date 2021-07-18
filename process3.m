8a480aa2a0b7205ad0364c4205ea8d0ed620b72c
Alby0n <73681462+Alby0n@users.noreply.github.com>
2021-07-18 16:52:12 +0530

    Update process3.m

--- process3.m
+++ process3.m
@@ -1,16 +1,9 @@
-stri="this is a email email email EMAILemail email email email emailemail";
+string = " Shrish is powerful email.com";
+function [count1] = process(string)
+clc
+stri=string;
 y=count1("email",1,stri);
 disp(y)
-
-function [c]=count1(Word,op,str)
-    c=count(str,Word,'IgnoreCase',true);
-    if c>15
-        c=15;
-    end
-    if op==1
-        disp("    spam");
-    else
-        disp("    ham");
-    end
+process3a(string,0,true);
 end
     
