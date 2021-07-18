1fd2fbca8321df2930a094ecbc05ec9a84868570
Alby0n <73681462+Alby0n@users.noreply.github.com>
2021-07-18 16:50:02 +0530

    Update process2.m

--- process2.m
+++ process2.m
@@ -1,20 +1,16 @@
-function [priorSpam, priorNonSpam] = process2()
+function [priorSpam, priorNonSpam,X_train_class_1,X_train_class_0] = process2()
   clc
   M = readtable('lingSpam.csv');
   X_train_class_1 = [];
   X_train_class_0 = [];
   for i=2:height(M)
       if M{i,3}==1
-          X_train_class_1=[X_train_class_1;M(i,1:3)];
+          X_train_class_1=[X_train_class_1;M(i,2:3)];
       else
-          X_train_class_0 = [X_train_class_0;M(i,1:3)];
+          X_train_class_0 = [X_train_class_0;M(i,2:3)];
       end      
   end
 
   priorSpam = height(X_train_class_1)/height(M);
   priorNonSpam = height(X_train_class_0)/height(M);
-endfunction
-
-[x, y] = process2();
-x
-y
+end
