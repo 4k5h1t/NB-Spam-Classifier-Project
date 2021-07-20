7a9ad1d908ecedbb1a96a20c890f990721d0a6a5
Akshit S <73750950+4k5h1t@users.noreply.github.com>
2021-07-20 20:11:45 +0530

    Process 1
    Done by Neha

---
+++ processing.m
@@ -0,0 +1,16 @@
+string="hello, h+o+w are you";
+string=lower(string);
+punctuation=[".",",","!",":",";","'","(",")","?","/","*","&","$","@","=","+","%","[","]","{","}"];
+string=erase(string,punctuation);
+words=split(string);
+words=transpose(words)
+x=["hello", "how","a","ok"];
+%Articles = ["a","an","the"];
+%Preposition = ["above","across","against","along","among","under","over","on","in","at","to","by","near","with","up","down","for","through","from","toward","since","into","onto","upon","from","considering","during","provided","concerning","following","including","regarding","during"];
+%Pronoun = ["i","me","he","she","you","we","us","it","him","her","they","them","anyone","nobody","somebody","someone","something","other","whichever","whoever","this","all","such","myself","yourself","himself","herself","Itself","ourselves","yourselves", "themselves","his","that","these","those","mine","its","hers","his","yours","ours","theirs","who","whom","whose","what","which","whichever","whoever","whomever","all","any","all","another","anybody","anyone","anything","both","each","either","everybody","everyone","everything","both","few","many","most","neither","nobody","none","nothing","one","other","others","several","some"];   
+%Conjunction = ["and","nor","but","or","yet","so","for","after","although","as","because","before","even","if","once","only","since","than ","though","till","unless","until","while","where","wherever","when","wherever","as","if","despite","after","before","Furthermore","However","therfore","consequently","firstly","secondly","thirdly","finaly","first","second","third","still","yet","nevertheless"];
+%Other_words = ["subject ","Dear ","Respected ","Sir ","Ma'am "];
+
+unwanted=["are","a","an","the","above","across","against","along","among","under","over","on","in","at","to","by","near","with","up","down","for","through","from","toward","since","into","onto","upon","from","considering","during","provided","concerning","following","including","regarding","during","i","me","he","she","you","we","us","it","him","her","they","them","anyone","nobody","somebody","someone","something","other","whichever","whoever","this","all","such","myself","yourself","himself","herself","Itself","ourselves","yourselves", "themselves","his","that","these","those","mine","its","hers","his","yours","ours","theirs","who","whom","whose","what","which","whichever","whoever","whomever","all","any","all","another","anybody","anyone","anything","both","each","either","everybody","everyone","everything","both","few","many","most","neither","nobody","none","nothing","one","other","others","several","some","and","nor","but","or","yet","so","for","after","although","as","because","before","even","if","once","only","since","than ","though","till","unless","until","while","where","wherever","when","wherever","as","if","despite","after","before","Furthermore","However","therfore","consequently","firstly","secondly","thirdly","finaly","first","second","third","still","yet","nevertheless","subject ","Dear ","Respected ","Sir ","Ma'am "];
+
+pure = setdiff(words,unwanted) %only take different values of 2 lists
